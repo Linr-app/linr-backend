a31caa4f917b82033549137eb740e7f6e2e24f3c
Ivan Dardi <ivy.dardi@gmail.com>
2017-09-30 23:48:12 -0300

    Add production changes

--- backend/config/index.js
+++ backend/config/index.js
@@ -7,7 +7,6 @@ const config = {
   env: process.env.NODE_ENV || "production",
   version: version,
   webPort: 3000,
-  bindingHost: "127.0.0.1",
   debug: process.env.NODE_ENV === "development" || process.env.NODE_ENV === "test",
   logdir: path.join(root, "logs"),
   enableCompress: false,
