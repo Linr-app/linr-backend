95a9cc9901ec61ae2cc144df07c71c53a00fbcd3
Igor Luppi <igor.luppi@gmail.com>
2017-12-04 11:09:05 -0200

    Update restaurante.js

--- backend/database/queries/restaurante.js
+++ backend/database/queries/restaurante.js
@@ -43,30 +43,33 @@ module.exports.updateRestaurante = (id, restaurante) => {
 
 /**
  * Atualiza o valor de uma avaliação de um restaurante feita por um usuario. 
- * @param avaliacao Objeto contendo id_restaurante[int], id_usuario_cadastrado[int] e valor[int]
+ * @param id_restaurante[int]
+ * @param id_usuario_cadastrado[int]
+ * @param valor[int]
  */
-module.exports.updateAvaliacao = avaliacao => {
+module.exports.updateAvaliacao = (id_restaurante, id_usuario_cadastrado, valor) => {
   return knex('avaliacao')
     .update({
-      'valor': parseInt(avaliacao.valor),
+      'valor': parseInt(valor),
     })
     .where({
-      'id_restaurante': parseInt(avaliacao.id_restaurante),
-      'id_usuario': parseInt(avaliacao.id_usuario_cadastrado),
+      'id_restaurante': parseInt(id_restaurante),
+      'id_usuario': parseInt(id_usuario_cadastrado),
     })
     .returning('*')
 }
 
 /**
  * Pega a média da avaliação de um restaurante. 
- * @param ids Objeto contendo id_restaurante[int] e id_usuario_cadastrado[int]
+ * @param id_restaurante[int]
+ * @param id_usuario_cadastrado[int]
  */
-module.exports.getAvaliacao = ids => {
+module.exports.getAvaliacao = (id_restaurante, id_usuario_cadastrado) => {
   return knex('avaliacao')
     .select('valor')
     .where({
-      'id_restaurante': parseInt(ids.id_restaurante),
-      'id_usuario': parseInt(ids.id_usuario_cadastrado),
+      'id_restaurante': parseInt(id_restaurante),
+      'id_usuario': parseInt(id_usuario_cadastrado),
     })
 }
 
@@ -84,14 +87,16 @@ module.exports.getAverageAvaliacao = id_restaurante => {
 
 /**
  * Cria uma nova avaliacao com um objeto avaliacao.
- * @param avaliacao Objeto contendo id_restaurante[int], id_usuario_cadastrado[int] e valor[int]
+ * @param id_restaurante[int]
+ * @param id_usuario_cadastrado[int]
+ * @param valor[int]
  */
-module.exports.createAvaliacao = avaliacao => {
+module.exports.createAvaliacao = (id_restaurante, id_usuario_cadastrado, valor) => {
   return knex('avaliacao')
     .insert({
-      id_restaurante: parseInt(avaliacao.id_restaurante),
-      id_usuario: parseInt(avaliacao.id_usuario_cadastrado),
-      valor: parseInt(avaliacao.valor),
+      id_restaurante: parseInt(id_restaurante),
+      id_usuario: parseInt(id_usuario_cadastrado),
+      valor: parseInt(valor),
     })
     .returning('*')
 }
