f49466c2e968115cb47531efdc836068992cbdea
Igor Luppi <igor.luppi@gmail.com>
2017-12-10 16:10:37 -0200

    Update fila.js

--- backend/routes/fila.js
+++ backend/routes/fila.js
@@ -5,13 +5,34 @@ const queries = require('../database/queries/fila')
 const router = new Router()
 
 /**
- GET    /fila        Return ALL filas
- GET    /fila/:id    Return a SINGLE fila
- POST   /fila        Add a fila
- PUT    /fila/:id    Update a fila
- DELETE /fila/:id    Delete a fila
+ *
+ * @module Filas
  */
 
+/**
+ * Retorna todas as filas presentes no banco.
+ *
+ * Formato de retorno:
+ * ```json
+ * [
+ *   {
+ *      id: int,
+ *      id_restaurante: int,
+ *      hora_funcionamento_inicio: Optional[String],
+ *      hora_funcionamento_fim: Optional[String],
+ *      tempo_medio_inicial: String,
+ *      descricao: Optional[String]
+ *   },
+ *   ...
+ * ]
+ * ```
+ *
+ * @function
+ * @inner
+ * @memberof module:Filas
+ * @name GET /filas
+ * @return Informacoes de todas as filas
+ */
 router.get('/', async ctx => {
   try {
     ctx.body = {
@@ -23,6 +44,42 @@ router.get('/', async ctx => {
   }
 })
 
+/**
+ * Retorna uma fila especifica com ID `id`
+ *
+ * Formato de retorno:
+ * ```json
+ * {
+ *   id: int,
+ *   id_restaurante: int,
+ *   hora_funcionamento_inicio: Optional[String],
+ *   hora_funcionamento_fim: Optional[String],
+ *   tempo_medio_inicial: String,
+ *   descricao: Optional[String],
+ *   usuarios_na_fila: {
+ *     id: int,
+ *     id_usuario: int,
+ *     nome: String,
+ *     telefone: String,
+ *     fcmtoken: String,
+ *     hora_entrada_fila: String,
+ *     hora_entrada_atendimento: Optional[String],
+ *     hora_saida_restaurante: Optional[String],
+ *     qtd_pessoas: int,
+ *     tem_reserva: bool,
+ *     desistiu_da_fila: bool
+ *   }
+ * }
+ * ```
+ *
+ * @function
+ * @inner
+ * @memberof module:Filas
+ * @name GET /filas/:id
+ * @param {Object} URL Parametros de URL
+ * @param {int} URL.id ID da fila
+ * @return Informacoes da fila com ID `id`
+ */
 router.get('/:id', async ctx => {
   try {
     const fila = await queries.getSingleFila(ctx.params.id)
@@ -43,6 +100,35 @@ router.get('/:id', async ctx => {
   }
 })
 
+/**
+ * Cria uma nova fila
+ *
+ * Formato de retorno:
+ * ```json
+ * [
+ *   {
+ *      id: int,
+ *      id_restaurante: int,
+ *      hora_funcionamento_inicio: Optional[String],
+ *      hora_funcionamento_fim: Optional[String],
+ *      tempo_medio_inicial: String,
+ *      descricao: Optional[String]
+ *   },
+ *   ...
+ * ]
+ * ```
+ *
+ * @function
+ * @inner
+ * @memberof module:Filas
+ * @name POST /filas
+ * @param {Object} Body Parametros da request POST/PUT
+ * @param {int} Body.id_restaurante
+ * @param {String} Body.hora_funcionamento_inicio
+ * @param {String} Body.hora_funcionamento_fim
+ * @param {String} Body.tempo_medio_inicial 
+ * @return Informacoes da nova fila criada
+ */
 router.post('/', async ctx => {
   try {
     const fila = await queries.addFila(ctx.request.body)
@@ -52,7 +138,7 @@ router.post('/', async ctx => {
         status: 'ok',
         data: fila,
       }
-      const data = new Date(ctx.body.tempo_medio_inicial)
+	     const data = new Date(ctx.body.tempo_medio_inicial)
       const tempo_medio_inicial = data.getHours()*60*60 + data.getMinutes()*60 + data.getSeconds()
       await axios.post(`https://linrapp-prediction.herokuapp.com/register/${ctx.params.id}`, {
         tempo_medio_inicial: tempo_medio_inicial,
@@ -69,6 +155,36 @@ router.post('/', async ctx => {
   }
 })
 
+/**
+ * Edita a fila com o ID `id`
+ *
+ * Formato de retorno:
+ * ```json
+ * [
+ *   {
+ *      id: int,
+ *      id_restaurante: int,
+ *      hora_funcionamento_inicio: Optional[String],
+ *      hora_funcionamento_fim: Optional[String],
+ *      tempo_medio_inicial: String,
+ *      descricao: Optional[String]
+ *   },
+ *   ...
+ * ]
+ * ```
+ *
+ * @function
+ * @inner
+ * @memberof module:Filas
+ * @name PUT /filas/:id
+ * @param {Object} URL Parametros de URL
+ * @param {int} URL.id ID da fila
+ * @param {Object} Body Parametros da request POST/PUT
+ * @param {String} [Body.hora_funcionamento_inicio]
+ * @param {String} [Body.hora_funcionamento_fim]
+ * @param {int} [Body.tempo_medio_inicial] Tempo de espera medio em segundos
+ * @return Informacoes da fila alterada
+ */
 router.put('/:id', async ctx => {
   try {
     const fila = await queries.updateFila(ctx.params.id, ctx.request.body)
@@ -94,12 +210,23 @@ router.put('/:id', async ctx => {
   }
 })
 
+/**
+ * Adiciona um novo usuario na fila com o ID `id`
+ *
+ * @function
+ * @inner
+ * @memberof module:Filas
+ * @name POST /filas
+ * @param {String} [hora_funcionamento_inicio]
+ * @param {String} [hora_funcionamento_fim]
+ * @param {int} [tempo_medio_inicial] Tempo de espera medio em segundos
+ */
 router.put('/:id/enter', async ctx => {
   try {
     const usuario = {
       id_usuario: ctx.request.body.id_usuario,
       qtd_pessoas: ctx.request.body.qtd_pessoas,
-      posicao_qdo_entrou: ctx.request.body.posicao_qdo_entrou,
+      posicao_qdo_entrou: ctx.request.body.posicao_qdo_entrou
     }
     const novo_usuario = await queries.addUserToFila(ctx.params.id, usuario)
     ctx.status = 200
@@ -115,6 +242,54 @@ router.put('/:id/enter', async ctx => {
   }
 })
 
+/**
+ * Adiciona um novo usuario cadastrado a fila de ID `id` a partir do token.
+ *
+ * @function
+ * @inner
+ * @memberof module:Filas
+ * @name PUT /filas/:id/entercadastrado
+ * @param {int} id Novo id do usuario a ser criado
+ * @param {Object} URL Parametros de URL
+ * @param {int} URL.id ID da fila
+ * @param {Object} Body Parametros da request POST/PUT
+ * @param {int} [Body.session_token] Token da sessao
+ * @param {int} [Body.qtd_pessoas] Quantidade de pessoas junto com o usuario
+ * @param {int} [Body.posicao_qdo_entrou] Posicao que entrou na fila
+ */
+router.put('/:id/entercadastrado', async ctx => {
+  try {
+    const usuario = {
+      session_token: ctx.request.body.session_token,
+      qtd_pessoas: ctx.request.body.qtd_pessoas,
+      posicao_qdo_entrou: ctx.request.body.posicao_qdo_entrou
+    }
+    const novo_usuario = await queries.addUserCadastradoToFila(ctx.params.id, usuario)
+    ctx.status = 200
+    ctx.body = {
+      status: 'ok',
+    }
+  } catch (err) {
+    ctx.status = 400
+    ctx.body = {
+      status: 'error',
+      message: err.message || 'Ocorreu um erro no servidor',
+    }
+  }
+})
+
+/**
+ * Marca um usuario como desistido na fila com o ID `id`
+ *
+ * @function
+ * @inner
+ * @memberof module:Filas
+ * @name PUT /filas/:id/desistir
+ * @param {Object} URL Parametros de URL
+ * @param {int} URL.id ID da fila
+ * @param {Object} Body Parametros da request POST/PUT
+ * @param {int} [Body.id_usuario_fila]
+ */
 router.put('/:id/desistir', async ctx => {
   try {
     const id_usuario_fila = ctx.request.body.id_usuario_fila
@@ -132,6 +307,19 @@ router.put('/:id/desistir', async ctx => {
   }
 })
 
+/**
+ * Marca um usuario como saido na fila com o ID `id`
+ *
+ * @function
+ * @inner
+ * @memberof module:Filas
+ * @name PUT /filas/:id/sair
+ * @param {Object} URL Parametros de URL
+ * @param {int} URL.id ID da fila
+ * @param {Object} Body Parametros da request POST/PUT
+ * @param {int} [Body.id_usuario_fila]
+ * @param {int} [Body.posicao_qdo_entrou] Posicao que entrou na fila
+ */
 router.put('/:id/sair', async ctx => {
   try {
     const id_usuario = ctx.request.body.id_usuario_fila
@@ -140,19 +328,19 @@ router.put('/:id/sair', async ctx => {
     ctx.body = {
       status: 'ok',
     }
-   const dia = new Date()
-   const dia_da_semama = dia.getDay()
-   
-   const data = new Date(ctx.body.hora_entrada_fila)
-   const hora_de_entrada = data.getHours()*60*60 + data.getMinutes()*60 + data.getSeconds()
-   
-   const posicao = ctx.request.body.posicao_qdo_entrou
-   const hora_de_saida = dia.getHours()*60*60 + dia.getMinutes()*60 + dia.getSeconds()
-   
-   const tempo_de_espera_na_fila = hora_de_saida - hora_de_entrada
-   if (tempo_de_espera_na_fila < 0) { //Caso vire o dia
-     tempo_de_espera_na_fila = tempo_de_espera_na_fila + 1440*60
-   } 
+	const dia = new Date()
+	const dia_da_semama = dia.getDay()
+
+	const data = new Date(ctx.body.hora_entrada_fila)
+	const hora_de_entrada = data.getHours()*60*60 + data.getMinutes()*60 + data.getSeconds()
+
+	const posicao = ctx.request.body.posicao_qdo_entrou
+	const hora_de_saida = dia.getHours()*60*60 + dia.getMinutes()*60 + dia.getSeconds()
+
+	const tempo_de_espera_na_fila = hora_de_saida - hora_de_entrada
+	if (tempo_de_espera_na_fila < 0) { //Caso vire o dia
+	  tempo_de_espera_na_fila = tempo_de_espera_na_fila + 1440*60
+	} 
    
     await axios.post(`https://linrapp-prediction.herokuapp.com/fit/${ctx.params.id}`, {
       dia_da_semana: dia_da_semama,
